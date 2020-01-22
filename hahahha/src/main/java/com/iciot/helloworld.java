1253ddb4494e2d5df906a9c64493733fa2b45a15
glone <gc1992129@163.com>
2020-01-22 17:20:28 +0800

    第一次maven

---
+++ hahahha/src/main/java/com/iciot/helloworld.java
@@ -0,0 +1,12 @@
+package com.iciot;
+
+import java.util.Iterator;
+
+public class helloworld {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        for(int a1:a){
+            System.out.println(a1);
+        }
+    }
+}
